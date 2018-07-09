d5e3ba5200250c54f344bec2d61465d09df6882b
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-09 16:53:51 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -11,14 +11,3 @@ def my_each(array)
     end
     array
 end
-
-
-+def my_each (array)
--  # code here	+  i = 0 
-+  
-+  while i < array.length
-+  yield(array[i])
-+  i = i + 1 
-+end 
-+array
- end 	 end 
