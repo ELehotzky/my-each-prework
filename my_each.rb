419de5a1ebdbc5cd0c7aab141199a5a2e3f69f98
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-09 16:50:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,13 @@
-array = [0, 1, 2, 3, 4]
-
 def my_each(array)
   if array.length == 0 then
     "Empty array set."
   end
-  my_each(array) { |i|
-    i = 0 
-    while i < array.length {
-      i + 1
-    }
-    yield 
-  }
+  while i < array.length 
+    yield(array[i])
+      i += 1
+    end
+    array
+  end
 end
+
+
