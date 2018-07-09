62750ae86de984e1e2fdd640e4617e8f166de991
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-09 16:55:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+array = [1, 2, 3, 4]
+
+def my_each(array)
+  if array.length == 0 then
+    "Empty array set."
+  end
+  i = 0 
+  while i < array.length 
+    yield(array[i])
+      i += 1
+    end
+    array
 end
